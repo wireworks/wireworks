13c9a94e6e97379030c21fcbe14b984d88b56de6
Henrique Colini <henriquecolini@gmail.com>
2019-03-02 21:57:28 -0300

    Added copyToClipboard() helper function

--- src/core/helpers.ts
+++ src/core/helpers.ts
@@ -1,3 +1,24 @@
 export function id(id: string) {
 	return document.getElementById(id);
 }
+
+export function copyToClipboard(str: string, done: (success: boolean) => void): void {
+
+	let tmp = document.createElement("textarea");
+
+	tmp.style.position = 'fixed';
+	tmp.style.top = "0";
+	tmp.style.left = "0";
+	tmp.style.opacity = "0";
+	tmp.value = str;
+
+	document.body.appendChild(tmp);
+	tmp.focus();
+	tmp.select();
+
+	let success = document.execCommand('copy');
+	document.body.removeChild(tmp);
+
+	done(success);
+
+}
