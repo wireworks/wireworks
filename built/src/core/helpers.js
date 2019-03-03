5ad87bd12dc0625e826fbc994b657d546eaba906
Henrique Colini <henriquecolini@gmail.com>
2019-03-03 13:45:00 -0300

    Added isCharNumeric()/isStringNumeric()

--- built/src/core/helpers.js
+++ built/src/core/helpers.js
@@ -1,6 +1,7 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    exports.NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     /**
      * Shorthand for document.getElementById(id).
      * @param  {string} elementId String that specifies the ID value. Case-insensitive.
@@ -31,4 +32,24 @@ define(["require", "exports"], function (require, exports) {
         }
     }
     exports.copyToClipboard = copyToClipboard;
+    /**
+     * Returns whether a character is a number.
+     * @param  {string} str The character to be anyalized.
+    */
+    function isCharNumeric(str) {
+        return exports.NUMBERS.indexOf(str) !== -1;
+    }
+    exports.isCharNumeric = isCharNumeric;
+    /**
+     * Returns whether a string is composed of numbers only.
+     * @param  {string} str The string to be anyalized.
+     */
+    function isStringNumeric(str) {
+        for (var i = 0; i < str.length; i++) {
+            if (!isCharNumeric(str.charAt(i)))
+                return false;
+        }
+        return true;
+    }
+    exports.isStringNumeric = isStringNumeric;
 });
--- src/core/helpers.ts
+++ src/core/helpers.ts
@@ -1,3 +1,5 @@
+export const NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 /**
  * Shorthand for document.getElementById(id).
  * @param  {string} elementId String that specifies the ID value. Case-insensitive.
@@ -32,4 +34,24 @@ export function copyToClipboard(str: string, done?: (success: boolean) => void):
 		done(success);
 	}
 
+}
+
+/**
+ * Returns whether a character is a number.
+ * @param  {string} str The character to be anyalized.
+*/
+export function isCharNumeric(str: string): boolean {
+	return NUMBERS.indexOf(str) !== -1;
+}
+
+/**
+ * Returns whether a string is composed of numbers only.
+ * @param  {string} str The string to be anyalized.
+ */
+export function isStringNumeric(str: string): boolean {
+	for (let i = 0; i < str.length; i++) {
+		if (!isCharNumeric(str.charAt(i)))
+			return false;
+	}
+	return true;
 }
