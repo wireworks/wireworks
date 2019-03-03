33c2a474d128b27969aef1acb9244cee0babac3e
Henrique Colini <henriquecolini@gmail.com>
2019-03-03 17:04:16 -0300

    Added clamp() function

--- src/core/helpers.ts
+++ src/core/helpers.ts
@@ -55,3 +55,13 @@ export function isStringNumeric(str: string): boolean {
 	}
 	return true;
 }
+
+/**
+ * Returns a value, clamped between max and min.
+ * @param  {number} value The number to be clamped.
+ * @param  {number} min The minimum possible number.
+ * @param  {number} max The maximum possible number.
+ */
+export function clamp(value: number, min: number, max: number): number {
+	return Math.min(Math.max(value, min), max);
+};
