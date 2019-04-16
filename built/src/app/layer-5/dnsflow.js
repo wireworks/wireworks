a3269abb9ecd681166f96e6b6cf57bfd26fd609e
Henrique Colini <henriquecolini@gmail.com>
2019-04-16 00:01:11 -0300

    Working line movement

--- built/src/app/layer-5/dnsflow.js
+++ built/src/app/layer-5/dnsflow.js
@@ -1,4 +1,4 @@
-define(["require", "exports", "../../core/utils/dom", "../../core/networking/layers/layer-5/domain"], function (require, exports, dom_1, domain_1) {
+define(["require", "exports", "../../core/utils/dom", "../../core/networking/layers/layer-5/domain", "../../core/utils/math"], function (require, exports, dom_1, domain_1, math_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     // DNSFlow
@@ -9,12 +9,12 @@ define(["require", "exports", "../../core/utils/dom", "../../core/networking/lay
     var domainDOM = dom_1.id("domain");
     var canvasDOM = dom_1.id("canvas");
     var ctx = canvasDOM.getContext("2d");
+    var fixedDeltaTime = 20;
     var Node = /** @class */ (function () {
-        function Node(pos, width, heigth, side, fillStyle) {
+        function Node(pos, width, heigth, fillStyle) {
             this.pos = pos;
             this.width = width;
             this.height = heigth;
-            this.side = side;
             this.fillStyle = fillStyle; // tmp!
         }
         Node.prototype.draw = function () {
@@ -22,16 +22,91 @@ define(["require", "exports", "../../core/utils/dom", "../../core/networking/lay
             ctx.fillStyle = this.fillStyle;
             ctx.fillRect(this.pos.x - (this.width / 2), this.pos.y - (this.height / 2), this.width, this.height);
         };
+        Node.prototype.getVertices = function () {
+            var x = this.pos.x;
+            var y = this.pos.y;
+            var w2 = this.width / 2;
+            var h2 = this.height / 2;
+            var a = { x: x - w2, y: y - h2 };
+            var b = { x: x + w2, y: y - h2 };
+            var c = { x: x + w2, y: y + h2 };
+            var d = { x: x - w2, y: y + h2 };
+            return { a: a, b: b, c: c, d: d };
+        };
+        Node.prototype.getOutput = function (side) {
+            var p = this.getVertices();
+            var f = 0.3;
+            var fw = f * this.width;
+            var fh = f * this.height;
+            switch (side) {
+                case "top":
+                    return { x: p.a.x + fw, y: p.a.y };
+                case "bottom":
+                    return { x: p.c.x - fw, y: p.c.y };
+                case "left":
+                    return { x: p.d.x, y: p.d.y - fh };
+                case "right":
+                    return { x: p.c.x, y: p.b.y + fh };
+            }
+        };
+        Node.prototype.getInput = function (side) {
+            var p = this.getVertices();
+            var f = 0.3;
+            var fw = f * this.width;
+            var fh = f * this.height;
+            switch (side) {
+                case "top":
+                    return { x: p.b.x - fw, y: p.a.y };
+                case "bottom":
+                    return { x: p.d.x + fw, y: p.c.y };
+                case "left":
+                    return { x: p.d.x, y: p.a.y + fh };
+                case "right":
+                    return { x: p.c.x, y: p.c.y - fh };
+            }
+        };
         return Node;
     }());
     var Line = /** @class */ (function () {
-        function Line(from, to, time, strokeStyle) {
+        function Line(from, to, time, strokeStyle, strokeWidth) {
             this.from = from;
             this.to = to;
             this.time = time;
             this.strokeStyle = strokeStyle;
+            this.lineWidth = strokeWidth;
         }
         Line.prototype.draw = function () {
+            var offX = this.from.pos.x - this.to.pos.x;
+            var offY = this.from.pos.y - this.to.pos.y;
+            var fromPoint;
+            var toPoint;
+            if (Math.abs(offX) > Math.abs(offY)) {
+                if (offX > 0) {
+                    fromPoint = this.from.getOutput("left");
+                    toPoint = this.to.getInput("right");
+                }
+                else {
+                    fromPoint = this.from.getOutput("right");
+                    toPoint = this.to.getInput("left");
+                }
+            }
+            else {
+                if (offY > 0) {
+                    fromPoint = this.from.getOutput("top");
+                    toPoint = this.to.getInput("bottom");
+                }
+                else {
+                    fromPoint = this.from.getOutput("bottom");
+                    toPoint = this.to.getInput("top");
+                }
+            }
+            ctx.beginPath();
+            ctx.strokeStyle = this.strokeStyle;
+            ctx.lineWidth = this.lineWidth;
+            ctx.lineCap = "round";
+            ctx.moveTo(fromPoint.x, fromPoint.y);
+            ctx.lineTo(fromPoint.x + (this.time * (toPoint.x - fromPoint.x)), fromPoint.y + (this.time * (toPoint.y - fromPoint.y)));
+            ctx.stroke();
         };
         return Line;
     }());
@@ -57,6 +132,11 @@ define(["require", "exports", "../../core/utils/dom", "../../core/networking/lay
             else {
                 var tmpRoot = new domain_1.Domain(".", undefined);
                 var domain = domain_1.Domain.extractDomain(tmpRoot, fullName);
+                connectNodes(hostNode, localNode, "#b0db8a", 10, 500, function () {
+                    connectNodes(localNode, rootNode, "#b0db8a", 10, 500, function () {
+                        connectNodes(rootNode, localNode, "#db938a", 10, 500);
+                    });
+                });
             }
         }
         catch (error) {
@@ -81,22 +161,43 @@ define(["require", "exports", "../../core/utils/dom", "../../core/networking/lay
             errorWrapperDOM.appendChild(table);
         }
     }
+    function connectNodes(from, to, strokeStyle, lineWidth, delay, callback) {
+        if (callback === void 0) { callback = undefined; }
+        var spent = 0;
+        var line = new Line(from, to, 0, strokeStyle, lineWidth);
+        drawables.push(line);
+        var interval = setInterval(function () {
+            line.time = math_1.clamp(spent / delay, 0, 1);
+            render();
+            spent += fixedDeltaTime;
+            if (spent >= delay) {
+                line.time = 1;
+                render();
+                if (callback) {
+                    callback();
+                }
+                clearInterval(interval);
+            }
+        }, fixedDeltaTime);
+    }
     function render() {
+        ctx.clearRect(0, 0, canvasDOM.width, canvasDOM.height);
         for (var i = 0; i < drawables.length; i++) {
             drawables[i].draw();
         }
     }
-    function setupCanvas() {
+    function resetCanvas() {
         var px = 50; // padding
         var py = 50;
         var w = canvasDOM.width;
         var h = canvasDOM.height;
-        hostNode = new Node({ x: px, y: h - py }, 60, 60, "top", "#FF0000");
-        localNode = new Node({ x: px, y: h / 2 }, 60, 60, "top", "#FFFF00");
-        rootNode = new Node({ x: px, y: py }, 60, 60, "top", "#00FF00");
-        interNode = new Node({ x: w / 2, y: h / 2 }, 60, 60, "top", "#FF00FF");
-        adminNode = new Node({ x: w - px, y: h / 2 }, 60, 60, "top", "#00FFFF");
-        destNode = new Node({ x: w - px, y: h - py }, 60, 60, "top", "#0000FF");
+        hostNode = new Node({ x: px, y: h - py }, 60, 60, "#FF0000");
+        localNode = new Node({ x: px, y: h / 2 }, 60, 60, "#FFFF00");
+        rootNode = new Node({ x: px, y: py }, 60, 60, "#00FF00");
+        interNode = new Node({ x: w / 2, y: h / 2 }, 60, 60, "#FF00FF");
+        adminNode = new Node({ x: w - px, y: h / 2 }, 60, 60, "#00FFFF");
+        destNode = new Node({ x: w - px, y: h - py }, 60, 60, "#0000FF");
+        drawables = [];
         drawables.push(hostNode);
         drawables.push(localNode);
         drawables.push(rootNode);
@@ -104,7 +205,7 @@ define(["require", "exports", "../../core/utils/dom", "../../core/networking/lay
         drawables.push(adminNode);
         drawables.push(destNode);
     }
-    setupCanvas();
+    resetCanvas();
     render();
     dom_1.id("run").addEventListener("click", function (ev) {
         run();
--- src/app/layer-5/dnsflow.ts
+++ src/app/layer-5/dnsflow.ts
@@ -1,5 +1,6 @@
 import { id } from "../../core/utils/dom";
 import { Domain, ERROR_INVALID_LABEL, ERROR_FULL_NAME_RANGE } from "../../core/networking/layers/layer-5/domain";
+import { clamp } from "../../core/utils/math";
 
 // DNSFlow
 // +=========================+
@@ -10,6 +11,7 @@ const errorWrapperDOM = id("error_wrapper");
 const domainDOM = id("domain");
 const canvasDOM = <HTMLCanvasElement>id("canvas");
 const ctx = canvasDOM.getContext("2d");
+const fixedDeltaTime = 20;
 
 type Point = {	x: number, y: number }
 
@@ -24,14 +26,12 @@ class Node implements Drawable {
 	public pos: Point;
 	public width: number;
 	public height: number;
-	public side: "top"|"bottom"|"left"|"right";
 	public fillStyle: string;
 
-	constructor(pos: Point, width: number, heigth: number, side: "top" | "bottom" | "left" | "right", fillStyle: string) {
+	constructor(pos: Point, width: number, heigth: number, fillStyle: string) {
 		this.pos = pos;
 		this.width = width;
 		this.height = heigth;
-		this.side = side;
 		this.fillStyle = fillStyle; // tmp!
 	}
 
@@ -43,6 +43,66 @@ class Node implements Drawable {
 
 	}
 
+	public getVertices(): {a: Point, b: Point, c: Point, d: Point} {
+
+		let x = this.pos.x;
+		let y = this.pos.y;
+		let w2 = this.width / 2;
+		let h2 = this.height / 2;
+
+		let a = { x: x - w2, y: y - h2 };
+		let b = { x: x + w2, y: y - h2 };
+		let c = { x: x + w2, y: y + h2 };
+		let d = { x: x - w2, y: y + h2 };
+
+		return {a,b,c,d};
+
+	}
+
+	public getOutput(side: "top" | "bottom" | "left" | "right"): Point {
+
+		let p = this.getVertices();
+		let f = 0.3;
+		let fw = f * this.width;
+		let fh = f * this.height;
+
+		switch (side) {
+
+			case "top":
+				return { x: p.a.x + fw, y: p.a.y };
+			case "bottom":
+				return { x: p.c.x - fw, y: p.c.y };
+			case "left":
+				return { x: p.d.x, y: p.d.y - fh };
+			case "right":
+				return { x: p.c.x, y: p.b.y + fh };
+
+		}
+
+	}
+
+	public getInput(side: "top" | "bottom" | "left" | "right"): Point {
+
+		let p = this.getVertices();
+		let f = 0.3;
+		let fw = f * this.width;
+		let fh = f * this.height;
+
+		switch (side) {
+
+			case "top":
+				return { x: p.b.x - fw, y: p.a.y };
+			case "bottom":
+				return { x: p.d.x + fw, y: p.c.y };
+			case "left":
+				return { x: p.d.x, y: p.a.y + fh };
+			case "right":
+				return { x: p.c.x, y: p.c.y - fh };
+
+		}
+
+	}
+
 }
 
 class Line implements Drawable {
@@ -51,17 +111,56 @@ class Line implements Drawable {
 	public to: Node;
 	public time: number;
 	public strokeStyle: string;
+	public lineWidth: number;
 
-	constructor(from: Node, to: Node, time: number, strokeStyle: string) {
+	constructor(from: Node, to: Node, time: number, strokeStyle: string, strokeWidth: number) {
 
 		this.from = from;
 		this.to = to;
 		this.time = time;
 		this.strokeStyle = strokeStyle;
+		this.lineWidth = strokeWidth;
 
 	}
 
 	public draw(): void {
+
+		let offX = this.from.pos.x - this.to.pos.x;
+		let offY = this.from.pos.y - this.to.pos.y;
+
+		let fromPoint: Point;
+		let toPoint: Point;
+
+		if (Math.abs(offX) > Math.abs(offY)) {
+			if (offX > 0) {
+				fromPoint = this.from.getOutput("left");
+				toPoint = this.to.getInput("right");
+			}
+			else {
+				fromPoint = this.from.getOutput("right");
+				toPoint = this.to.getInput("left");
+			}
+		}
+		else {
+			if (offY > 0) {
+				fromPoint = this.from.getOutput("top");
+				toPoint = this.to.getInput("bottom");
+			}
+			else {
+				fromPoint = this.from.getOutput("bottom");
+				toPoint = this.to.getInput("top");
+			}
+		}
+
+		ctx.beginPath();
+		ctx.strokeStyle = this.strokeStyle;
+		ctx.lineWidth = this.lineWidth;
+		ctx.lineCap = "round";
+
+		ctx.moveTo(fromPoint.x, fromPoint.y);
+		ctx.lineTo(fromPoint.x + (this.time * (toPoint.x - fromPoint.x)), fromPoint.y + (this.time * (toPoint.y - fromPoint.y)));
+
+		ctx.stroke();
 
 	}
 
@@ -97,6 +196,12 @@ function run() {
 			
 			let tmpRoot = new Domain(".", undefined);
 			let domain = Domain.extractDomain(tmpRoot, fullName);
+
+			connectNodes(hostNode, localNode, "#b0db8a", 10, 500, function() {
+				connectNodes(localNode, rootNode, "#b0db8a", 10, 500, function () {
+					connectNodes(rootNode, localNode, "#db938a", 10, 500);
+				});
+			});
 			
 		}
 
@@ -136,7 +241,39 @@ function run() {
 
 }
 
+function connectNodes(from: Node, to: Node, strokeStyle: string, lineWidth: number, delay: number, callback: Function = undefined) {
+
+	let spent = 0;
+	let line = new Line(from, to, 0, strokeStyle, lineWidth);
+	drawables.push(line);
+
+	let interval = setInterval(function(){
+
+		line.time = clamp(spent / delay, 0, 1);
+
+		render();
+
+		spent += fixedDeltaTime;
+		
+		if (spent >= delay) {
+
+			line.time = 1;
+			render();
+
+			if (callback) {
+				callback();
+			}
+
+			clearInterval(interval);
+		}
+
+	}, fixedDeltaTime);
+
+}
+
 function render() {
+
+	ctx.clearRect(0, 0, canvasDOM.width, canvasDOM.height);
 
 	for (let i = 0; i < drawables.length; i++) {
 		drawables[i].draw();	
@@ -144,19 +281,21 @@ function render() {
 
 }
 
-function setupCanvas() {
+function resetCanvas() {
 
 	let px = 50; // padding
 	let py = 50;
 	let w = canvasDOM.width;
 	let h = canvasDOM.height;
 	
-	hostNode = new Node({x: px, y: h - py},60,60,"top", "#FF0000");
-	localNode = new Node({ x: px, y: h / 2 }, 60, 60, "top", "#FFFF00");
-	rootNode = new Node({ x: px, y: py }, 60, 60, "top", "#00FF00");
-	interNode = new Node({ x: w / 2, y: h / 2 }, 60, 60, "top", "#FF00FF");
-	adminNode = new Node({ x: w - px, y: h / 2 }, 60, 60, "top", "#00FFFF");
-	destNode = new Node({ x: w - px, y: h - py }, 60, 60, "top", "#0000FF");
+	hostNode = new Node({x: px, y: h - py},60,60,"#FF0000");
+	localNode = new Node({ x: px, y: h / 2 }, 60, 60, "#FFFF00");
+	rootNode = new Node({ x: px, y: py }, 60, 60, "#00FF00");
+	interNode = new Node({ x: w / 2, y: h / 2 }, 60, 60, "#FF00FF");
+	adminNode = new Node({ x: w - px, y: h / 2 }, 60, 60, "#00FFFF");
+	destNode = new Node({ x: w - px, y: h - py }, 60, 60, "#0000FF");
+
+	drawables = [];
 
 	drawables.push(hostNode);
 	drawables.push(localNode);
@@ -167,7 +306,7 @@ function setupCanvas() {
 
 }
 
-setupCanvas();
+resetCanvas();
 render();
 
 id("run").addEventListener("click", function(ev: MouseEvent) {
