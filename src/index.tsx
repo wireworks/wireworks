772281fd555e2f31eef5782be411d8ee50141f60
Alvaro Dziadzio <dziadzio500@yahoo.com.br>
2019-06-15 21:17:27 -0300

    new config

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker"
+import ReactDOM from "react-dom";
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<h1>Wireworks</h1>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
