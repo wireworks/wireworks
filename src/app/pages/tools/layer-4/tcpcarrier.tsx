a4dceba179c9e5a572331da2af13faa48ee2047d
Alvaro Dziadzio <dziadzio.alvaro@gmail.com>
2019-09-01 21:16:45 -0300

    progress

--- src/app/pages/tools/layer-4/tcpcarrier.tsx
+++ src/app/pages/tools/layer-4/tcpcarrier.tsx
@@ -1,13 +1,35 @@
 import React, { ChangeEvent, Component, FC } from "react";
 import variables from "src/sass/pages/tcpcarrier.scss";
 import "src/sass/pages/tcpcarrier.scss";
+import anime from "animejs";
 
 
+enum TcpFlags {
+    none = 0,
+    NS   = 1 << 0,
+    CWR  = 1 << 1,
+    ECE  = 1 << 2,
+    URG  = 1 << 3,
+    ACK  = 1 << 4,
+    PSH  = 1 << 5,
+    RST  = 1 << 6,
+    SYN  = 1 << 7,
+    FIN  = 1 << 8,
+}
 
-const TcpPacket: FC = () =>
+class TcpHeader {
+    sequence: number;
+    acknowledgment: number;
+    flags: number;
+    windowSize: number;
+    data: string;
+}
+
+
+const TcpPacket: FC<{id: number}> = (props) =>
 
 <div className="tcp-p-rail">
-    <div className="tcp-p-slider">
+    <div id={"packet-" + props.id} className="tcp-p-slider">
         <div className="tcp-p-ball"></div>
     </div>
     <div className="tcp-p-ball tcp-p-hint tcp-left"></div>
@@ -16,15 +38,47 @@ const TcpPacket: FC = () =>
 
 
 
-class TcpCarrier extends Component {
+class TcpCarrier extends Component<{}, {stream1: string,
+stream2: string,
+window1: number,
+window2: number}> {
 
-    test = () => {
-        document.getElementsByClassName("tcp-p-slider")[0].style.transform = "translateX(100%)";
+    state = {
+        stream1: "abcdefghijklmnopqrstuvwxyz",
+        stream2: "",
+        window1: 0,
+        window2: 0
+    }
+
+    start = () => {
+
+        anime.set("#packet-0", {
+            translateX: "0%"
+        });
+
+        let a = anime({
+            targets: "#packet-0",
+            translateX: "100%",
+            easing: "linear",
+            duration: 700,
+            delay: 0,
+            complete: () => {
+                this.setState((st) => {return {window2: st.window2+1}});
+            }
+        });
+
     }
 
     //////////////////////////////////////////////////////////////////
 
     render() {
+
+
+        let arr = Array(5);
+        for (let i = 0; i < 5; i++) {
+            arr[i] = <TcpPacket id={i}/>
+        }
+
         return (
             <main id="tcp-carrier">
 
@@ -32,22 +86,22 @@ class TcpCarrier extends Component {
                 <div className="tcp-cont-packet">
 
                     <div className="tcp-cont-window">
-                        <div className="tcp-window-wrapper tcp-left">
+                        <div className="tcp-window-wrapper tcp-left" style={{transform: `translateY(${this.state.window1*100}%)`}}>
                             <div className="tcp-window"></div>
                         </div>
-                        <div className="tcp-window-wrapper tcp-right">
+                        <div className="tcp-window-wrapper tcp-right" style={{transform: `translateY(${this.state.window2*100}%)`}}>
                             <div className="tcp-window"></div>
                         </div>
                         
                     </div>
 
-                    {Array(20).fill(<TcpPacket/>)}
+                    {arr}
                 </div>
 
                 {/* Menu */}
                 <div className="tcp-cont-menu">
                     <div>
-                        <button onClick={this.test}>Start</button>
+                        <button onClick={this.start}>Start</button>
                         <button>Stop</button>
                     </div>
                 </div>
