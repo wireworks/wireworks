009b864d557263131bf447054de6004eb6ef8cd0
Alvaro Dziadzio <dziadzio.alvaro@gmail.com>
2019-09-17 20:30:57 -0300

    response

--- src/app/pages/tools/layer-4/tcpcarrier.tsx
+++ src/app/pages/tools/layer-4/tcpcarrier.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, Component, FC } from "react";
 import variables from "src/sass/pages/tcpcarrier.scss";
 import "src/sass/pages/tcpcarrier.scss";
-import anime from "animejs";
+import anime, { AnimeAnimParams, AnimeInstance } from "animejs";
 
 
 enum TcpFlags {
@@ -29,44 +29,65 @@ class TcpHeader {
 const TcpPacket: FC<{id: number}> = (props) =>
 
 <div className="tcp-p-rail">
-    <div id={"packet-" + props.id} className="tcp-p-slider">
+    <div className="tcp-p-slider">
         <div className="tcp-p-ball"></div>
     </div>
-    <div className="tcp-p-ball tcp-p-hint tcp-left"></div>
-    <div className="tcp-p-ball tcp-p-hint tcp-right"></div>
+    <div className="tcp-p-ball tcp-p-hint tcp-left">{props.id}</div>
+    <div className="tcp-p-ball tcp-p-hint tcp-right">{props.id}</div>
 </div>
 
 
+interface ITcpCarrier {
+    progress: Array<number>
+}
 
-class TcpCarrier extends Component<{}, {stream1: string,
-stream2: string,
-window1: number,
-window2: number}> {
+class TcpCarrier extends Component<{}, ITcpCarrier> {
 
     state = {
-        stream1: "abcdefghijklmnopqrstuvwxyz",
-        stream2: "",
-        window1: 0,
-        window2: 0
+        progress: Array<number>(3).fill(2)
+    };
+
+    animation: AnimeInstance;
+
+    addProgress = (index: number) => {
+        let p = this.state.progress;
+        p[index]--;
+        this.setState({progress: p});
+    }
+
+    step = () => {
+
+        let reverse = true;
+        let ind = this.state.progress.findIndex((value) => value == 1);
+
+        if (ind == -1) {
+            ind = this.state.progress.findIndex((value) => value == 2);
+            reverse = false;
+        }
+        
+        if (ind != -1) {
+            let el = document.getElementsByClassName("tcp-p-slider")[ind];
+            anime({
+                targets: el,
+                translateX: ["0%", "100%"],
+                duration: 2000,
+                easing: "linear",
+                direction: reverse ? "reverse" : "normal",
+
+                complete: () => {
+                    this.addProgress(ind);
+                    let wind = document.getElementsByClassName("tcp-window-wrapper")[this.state.progress[ind]] as HTMLElement;
+                    wind.style.transform = `translateY(${(ind+1) * 100}%)`;
+                    this.step();
+                }
+
+            });
+        }
     }
 
     start = () => {
-
-        anime.set("#packet-0", {
-            translateX: "0%"
-        });
-
-        let a = anime({
-            targets: "#packet-0",
-            translateX: "100%",
-            easing: "linear",
-            duration: 700,
-            delay: 0,
-            complete: () => {
-                this.setState((st) => {return {window2: st.window2+1}});
-            }
-        });
-
+        this.step();
+        
     }
 
     //////////////////////////////////////////////////////////////////
@@ -74,8 +95,8 @@ window2: number}> {
     render() {
 
 
-        let arr = Array(5);
-        for (let i = 0; i < 5; i++) {
+        let arr = Array(20);
+        for (let i = 0; i < 20; i++) {
             arr[i] = <TcpPacket id={i}/>
         }
 
@@ -86,10 +107,10 @@ window2: number}> {
                 <div className="tcp-cont-packet">
 
                     <div className="tcp-cont-window">
-                        <div className="tcp-window-wrapper tcp-left" style={{transform: `translateY(${this.state.window1*100}%)`}}>
+                        <div className="tcp-window-wrapper tcp-left">
                             <div className="tcp-window"></div>
                         </div>
-                        <div className="tcp-window-wrapper tcp-right" style={{transform: `translateY(${this.state.window2*100}%)`}}>
+                        <div className="tcp-window-wrapper tcp-right">
                             <div className="tcp-window"></div>
                         </div>
                         
