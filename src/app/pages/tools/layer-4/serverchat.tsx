11b4b6f8246090f28466516e943cd2dc52967c73
henriquecolini <henriquecolini@gmail.com>
2019-09-26 10:21:59 -0300

    Style changes

--- src/app/pages/tools/layer-4/serverchat.tsx
+++ src/app/pages/tools/layer-4/serverchat.tsx
@@ -6,73 +6,83 @@
 import React, { Component, RefObject } from "react";
 import "src/sass/pages/serverchat.scss";
 
-type ChatMessage = {from: string, message: string};
+type ChatMessage = { from: string; message: string };
 
 class ServerChat extends Component {
+	state = {
+		chat: [] as ChatMessage[]
+	};
 
-    state = {
-        chat: [] as ChatMessage[]
-    }
+	componentDidMount() {
+		this.setState({
+			chat: [
+				{
+					from: "client",
+					message: "Olá servidor! Podemos começar uma conexão?"
+				},
+				{ from: "server", message: "Claro, cliente! Vamos começar?" },
+				{ from: "client", message: "Tudo bem, conexão iniciada..." }
+			]
+		});
+	}
 
-    componentDidMount () {
-        this.setState({chat: [
-            {from: "client", message: "Olá servidor! Podemos começar uma conexão?"},
-            {from: "server", message: "Claro, cliente! Vamos começar?"},
-            {from: "client", message: "Tudo bem, conexão iniciada..."}
-        ]});
-    }
-
-    render() {
-        return(
-            <main>
-                <div className="hbox">
-                    <ChatPanel displayName="Cliente" name="client" chat={this.state.chat}/>
-                    <ChatPanel displayName="Servidor" name="server" chat={this.state.chat}/>
-                </div>
-            </main>
-        );
-    }
-
+	render() {
+		return (
+			<main>
+				<div className="hbox">
+					<ChatPanel
+						displayName="Cliente"
+						name="client"
+						chat={this.state.chat}
+					/>
+					<ChatPanel
+						displayName="Servidor"
+						name="server"
+						chat={this.state.chat}
+					/>
+				</div>
+			</main>
+		);
+	}
 }
 
 interface ChatPanelProps {
-    className?: string,
-    displayName: string,
-    name: string,
-    chat: ChatMessage[];
+	className?: string;
+	displayName: string;
+	name: string;
+	chat: ChatMessage[];
 }
 
 class ChatPanel extends Component<ChatPanelProps> {
+	render() {
+		let messages = [];
 
-    render() {
+		for (let i = 0; i < this.props.chat.length; i++) {
+			const msg = this.props.chat[i];
+			messages[i] = (
+				<div className={"message-wrapper " + (msg.from === this.props.name ? "self" : "other")}>
+					<div className="message">{msg.message}</div>
+				</div>
+			);
+		}
 
-        let messages = [];
-
-        for (let i=0; i<this.props.chat.length; i++) {
-
-            const msg = this.props.chat[i];
-            messages[i] = <div className={"message " + (msg.from === this.props.name? "self" : "other")}>{ msg.message }</div>
-
-        }
-
-        return (
-            <div className={"chat-panel " + (this.props.className? this.props.className : "")}>
-                <header>
-                    <div className="subtitle">Visão do</div>
-                    <div className="name">{this.props.displayName}</div>
-                </header>
-                <div className="content">
-                    <div className="messages">
-                        {messages}
-                    </div>
-                    <div className="input">
-
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
+		return (
+			<div
+				className={
+					"chat-panel " + (this.props.className ? this.props.className : "")
+				}
+			>
+				<header>
+					<div className="subtitle">Visão do</div>
+					<div className="name">{this.props.displayName}</div>
+				</header>
+				<div className="content">
+					<div className="messages">{messages}</div>
+					<div className="input"></div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ServerChat;
