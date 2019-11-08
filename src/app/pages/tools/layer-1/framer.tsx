43186056bcb4f8fe192d6c5ac0c2afea29ba1920
Alvaro Dziadzio <dziadzio.alvaro@gmail.com>
2019-11-08 20:57:39 -0200

    framer data

--- src/app/pages/tools/layer-1/framer.tsx
+++ src/app/pages/tools/layer-1/framer.tsx
@@ -3,10 +3,30 @@ import React, { Component } from "react";
 
 class Framer extends Component {
 
+	state = {
+		data: new Array<{
+			num: number,
+			type: string
+		}>()
+	};
+
+	componentDidMount() {
+		const dt = this.state.data;
+
+		dt.push({num: 0b10101010, type: "preamble"});
+
+		this.setState({data: dt});
+
+	}
+
+
 	render() {
 		return (
 			<main>
 				<h1>Hello</h1>
+				<ul>
+					{this.state.data.map((it, k) => <li key={k}>{it.type + it.num}</li>)}
+				</ul>
 			</main>
 		); 
 	} 
