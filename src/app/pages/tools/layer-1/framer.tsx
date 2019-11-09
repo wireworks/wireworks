70b80dc937b34272305935c26d5b88b4abb56e06
Alvaro Dziadzio <dziadzio.alvaro@gmail.com>
2019-11-09 00:43:19 -0200

    random numer

--- src/app/pages/tools/layer-1/framer.tsx
+++ src/app/pages/tools/layer-1/framer.tsx
@@ -5,35 +5,60 @@ import "src/sass/pages/framer.scss";
 class Framer extends Component {
 
 	ty = ["Noise", "Preamble", "SFD", "Frame Data", "FCS"];
-	reg = /(A{14})(AB)(.*?)(0{24})/g
+	reg = /(A{14})(AB)(.*?)(0{24})/g;
+	reg2 = /.{2}/g;
+
+	start = "AAAAAAAAAAAAAAAB";
+	end = "000000000000000000000000";
 
 	state = {
-		data: "AAAAAAAAAAAAAAAB342423598381928392382398000000000000000000000000123123123123123AAAAAAAAAAAAAAAB3424235AAAA98381928392382398000000000000000000000000000AAAAAAAAAAAAAAAB342423598381"
+		data: "",
+		num: 0
 	};
 
 	componentDidMount() {
 
 	}
 
+	insert = (str: string) => {
+		let data = this.state.data;
+		data += str;
+		this.setState({data: data});
+	}
+
+	handleInput = (ev) => {
+		let val = ev.target.value > 255 ? 255 : ev.target.value;
+		val = val < 0 ? 0 : val;
+		this.setState({num: parseInt(val)});
+	}
+
 	render() {
 
-		let arr = new Array()
+		let arr = []
 
 		const segments = this.state.data.split(this.reg);
 		for (let ind in segments) {
 			const i = parseInt(ind);
 			const cName = "framer-" + this.ty[i%this.ty.length].replace(" ", "").toLowerCase();
-			if (segments[i] !== "")
-				arr.push(<span key={i} className={cName}>{segments[i]}</span>)
+			if (segments[i] !== "") {
+				const bits = segments[i].match(this.reg2)
+				for (let d in bits) {
+					arr.push(<span key={`s${i}-i${d}`} className={cName}>{bits[parseInt(d)]}</span>)
+				}
+			}
 		}
 
 		return (
 			<main>
-				<h1>Hello</h1>
+				<button onClick={() => this.insert(this.start)}>Inserir Inicio</button>
+				<button onClick={() => this.insert(this.end)}>Inserir Fim</button>
+				<input type="number" onChange={this.handleInput} value={this.state.num}/>
+				<button onClick={() => this.insert(this.state.num.toString(16).toUpperCase().padStart(2, "0"))}>Inserir</button>
+				<button onClick={() => this.insert(Math.floor(Math.random()*255).toString(16).toUpperCase().padStart(2, "0"))}>Inserir Aleatorio</button>
 				<div className="framer-container">
 					{arr}
 				</div>
-			</main>
+			</main>	
 		); 
 	} 
 
