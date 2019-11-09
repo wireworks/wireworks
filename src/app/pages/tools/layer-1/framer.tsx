67bf4ebaa3d7b043aaca20c63bf3f60b32b03378
Alvaro Dziadzio <dziadzio.alvaro@gmail.com>
2019-11-08 23:08:05 -0200

    framer data span

--- src/app/pages/tools/layer-1/framer.tsx
+++ src/app/pages/tools/layer-1/framer.tsx
@@ -3,29 +3,35 @@ import React, { Component } from "react";
 
 class Framer extends Component {
 
+	ty = ["Noise", "Preamble", "SFD", "Frame Data", "FCS"];
+	reg = /(A{14})(AB)(.*?)(0{24})/g
+
 	state = {
-		data: "AA"
+		data: "AAAAAAAAAAAAAAAB342423598381928392382398000000000000000000000000123123123123123AAAAAAAAAAAAAAAB3424235AAAA98381928392382398000000000000000000000000000AAAAAAAAAAAAAAAB342423598381"
 	};
 
 	componentDidMount() {
 
-		const reg = /(A{14})(AB)(.*?)(0{24})/g
-		const st = "AAAAAAAAAAAAAAAB342423598381928392382398000000000000000000000000123123123123123AAAAAAAAAAAAAAAB3424235AAAA98381928392382398000000000000000000000000000AAAAAAAAAAAAAAAB342423598381";
-
-		const ty = ["Noise", "Preamble", "SFD", "Frame Data", "FCS"];
-
-		const data = st.split(reg);
-		for (let i in data) {
-			console.log(ty[parseInt(i)%ty.length] + " " + data[parseInt(i)]);
-		}
-
 	}
 
 	render() {
+
+		let arr = new Array()
+
+		const segments = this.state.data.split(this.reg);
+		for (let ind in segments) {
+			const i = parseInt(ind);
+			const cName = "framer-" + this.ty[i%this.ty.length].replace(" ", "").toLowerCase();
+			if (segments[i] !== "")
+				arr.push(<span key={i} className={cName}>{segments[i]}</span>)
+		}
+
 		return (
 			<main>
 				<h1>Hello</h1>
-				
+				<div className="framer-container">
+					{arr}
+				</div>
 			</main>
 		); 
 	} 
